84595b6a8a9c62d382fe3ef5f8cb6ff952b0fa83
w410x <99654282+w410x@users.noreply.github.com>
2022-04-01 01:57:53 +0200

    pushing new javascript template after correcting webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+};
